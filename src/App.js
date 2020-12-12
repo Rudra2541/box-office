04a8ecb36e3baf648fa3a9a4101e9e41f0e39df0
enolaholmes <rudratambe@gmail.com>
2020-12-11 16:34:04 -0800

    Added Layout and Title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-     Hi
-    </div>
+    <Switch>
+     <Route exact path="/">
+      <Home />
+     </Route>
+     <Route exact path="/starred">
+       <Starred />
+     </Route>
+     <Route>
+       <div>Not Found</div>
+     </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+
+const Home = () => <MainPageLayout>this is home</MainPageLayout>;
+
+export default Home;
