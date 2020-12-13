82476eac01d019a8704a87448654d4352c9e70fe
enolaholmes <rudratambe@gmail.com>
2020-12-13 14:10:05 -0800

    Added show page and error messages

---
+++ src/pages/Show.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+  const [ show,setShow ] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+      if(isMounted) {
+      setShow(results);
+      setIsLoading(false);
+      }
+    }).catch(err => {
+      if(isMounted){
+      setError(err.message);
+      setIsLoading(false);
+      }
+    });
+
+    return () => {
+      isMounted = false;
+    }
+  }, [id]);
+  // eslint-disable-next-line no-console
+  console.log('show', show);
+
+  if(isLoading){
+    return <div>Data is loading</div>;
+  }
+  if(error){
+    return <div>Error Occured</div>
+  }
+  return <div>this is show page</div>;
+}
+
+export default Show
