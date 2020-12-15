25bc52cfc05d108ea154f5e5769411f29d8a062d
enolaholmes <rudratambe@gmail.com>
2020-12-14 17:55:19 -0800

    Styled the entire app

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+import { NavList, LinkStyled } from './Navs.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
   { to: '/starred', text: 'Starred' },
 ];
 
-const Navs = () => (
-    <div>
-      <ul>
+const Navs = () => {
+  const location = useLocation();
+    return (
+      <div>
+        <NavList>
         {LINKS.map(item => (
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
-    </div>
-  );
+      </NavList>
+      </div>
+    );
+  };
 
 export default Navs;
